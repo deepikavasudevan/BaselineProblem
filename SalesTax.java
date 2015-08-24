4e7b50da14a3776783c8b347e9394d413ee4a2ab
deepika <deepikav2793@gmail.com>
2015-08-24 17:42:23 +0530

    Added getItem method that terminates when user inputs 'Done'

---
+++ SalesTax.java
@@ -0,0 +1,30 @@
+/**
+ * Created by vdeepika on 24/08/15.
+ */
+import java.io.*;
+
+public class SalesTax {
+
+    String getItem() {
+        String totalItemDetails="";
+        try {
+            BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
+            totalItemDetails=br.readLine();
+        }
+        catch (IOException io) {
+            System.err.println("Error in input"+io);
+        }
+     return totalItemDetails;
+    }
+
+    public static void main(String[] args) {
+
+        SalesTax st=new SalesTax();
+        String item;
+
+        do {
+           item= st.getItem();
+        } while (!(item.equals("Done")));
+
+    }
+}
